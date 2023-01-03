fa20bc89407d80fc14d818a7027e569cb4f53ae2
wiczling.karol@gmail.com <wiczling.karol@gmail.com>
2023-01-03 21:26:46 +0100

    ads comment

--- src/class/gameMenager.ts
+++ src/class/gameMenager.ts
@@ -2,6 +2,7 @@ import Snake from "./snake";
 import CanvasMenager from "./canvasMenager";
 import ControlsMenager from "./controlsMenager";
 
+// Control the whole game
 export default class GameMenager {
   snake: Snake = new Snake();
   canvas: CanvasMenager = new CanvasMenager(1,1,1);
